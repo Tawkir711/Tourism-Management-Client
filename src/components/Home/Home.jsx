3b1505563e21fa7edc78cb2a486cf4e6bb0e7064
Tawkir711 <mdtawkirk711@gmail.com>
2024-06-09 19:14:26 +0600

    Updated slider description

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 
 const banner = () => {
   const [currentSlider, setCurrentSlider] = useState(0);
@@ -7,32 +8,70 @@ const banner = () => {
     {
       img: "https://i.postimg.cc/DZ8KR3HC/Mexico.jpg",
       title: "Mexico Tour Spot",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+      des: (
+        <p>
+          <i>
+            " An ancient Mayan pyramid surrounded by lush greenery in Mexico,
+            showcasing <br /> the rich cultural heritage and historical
+            significance of the region."
+          </i>
+        </p>
+      ),
     },
     {
       img: "https://i.postimg.cc/G2P6ZSt8/brazil.jpg",
       title: "Brazil Tour Spot",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+      des: (
+        <i>
+          "The iconic Christ the Redeemer statue overlooking the city of Rio de
+          Janeiro, Brazil, <br /> offering panoramic views and symbolizing peace
+          and hospitality."
+        </i>
+      ),
     },
     {
       img: "https://i.postimg.cc/W1DC24tP/peru.jpg",
       title: "Peru Tour Spot",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+      des: (
+        <i>
+          "The ancient Incan citadel of Machu Picchu in Peru, set high in the
+          Andes Mountains, <br /> known for its archaeological significance and
+          stunning views."
+        </i>
+      ),
     },
     {
       img: "https://i.postimg.cc/JhLq97kx/usa.jpg",
       title: "USA Tour Spot",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+      des: (
+        <i>
+          "The iconic Grand Canyon in Arizona, USA, with its vast, colorful, and{" "}
+          <br />
+          intricate landscape carved by the Colorado River."
+        </i>
+      ),
     },
     {
       img: "https://i.postimg.cc/hPHC9y4W/argentina.jpg",
       title: "Argentina Tour Spot",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+      des: (
+        <i>
+          "A breathtaking view of the Iguazu Falls, one of the largest and most
+          spectacular waterfall<br />  systems in the world, located on the
+          border of Argentina and Brazil."
+        </i>
+      ),
     },
     {
       img: "https://i.postimg.cc/gcDfs511/canada.jpg",
       title: "Canada Tour Spot",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+      des: (
+        <i>
+          "The stunning turquoise waters of Lake Louise in Banff National Park,
+          Canada, surrounded <br /> by towering snow-capped mountains and lush
+          forests."
+        </i>
+      ),
     },
   ];
   // if you don't want to change the slider automatically then you can just remove the useEffect
@@ -48,6 +87,9 @@ const banner = () => {
   }, [currentSlider]);
   return (
     <>
+      <Helmet>
+        <title>Tourism Management / Home</title>
+      </Helmet>
       <div
         className="w-full h-60 sm:h-96 md:h-[540px] flex flex-col items-center justify-center gap-5 lg:gap-10 bg-cover bg-center before:absolute before:bg-black/50 before:inset-0 transform duration-1000 ease-linear"
         style={{ backgroundImage: `url(${sliders[currentSlider].img})` }}
